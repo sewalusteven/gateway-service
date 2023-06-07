ef5ce2824cac50b52709fe00c7f09b5df4583f72
stevensewalu <ssewalu@projectcode.ug>
2023-06-07 09:08:51 +0300

    service and keys work

--- app/Models/ServiceKey.php
+++ app/Models/ServiceKey.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ServiceKey extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id','service_id','key'];
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+    public function services(): HasMany
+    {
+        return $this->hasMany(Service::class);
+    }
 }
